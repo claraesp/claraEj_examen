cbb2aea79e986c3c57767cb9f98ecb1ad4e73c64
ANDREU <ANDREU@PORTATILMANOLO>
2022-05-17 11:30:44 +0200

    He añadido una label

--- Ej_examen/Form1.Designer.cs
+++ Ej_examen/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -48,11 +49,22 @@
             this.textBox1.Size = new System.Drawing.Size(337, 23);
             this.textBox1.TabIndex = 1;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(253, 232);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(624, 299);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -66,5 +78,6 @@
 
         private Button button1;
         private TextBox textBox1;
+        private Label label1;
     }
 }
